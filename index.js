ea814b11ad97a50be210075c1fd6270387c03e89
SemenovYakov <d.simson241100@yandex.ru>
2022-05-11 20:40:01 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
+const command_text = require("./components/constants");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => {
+  ctx.replyWithSticker('CAACAgIAAxkBAAEEr-5ieQL1WxfzAqTkp2h4abIxHlLVJQACdAwAAra7OEuOzqfMQMSAJSQE')
+  console.log(ctx.message);
+});
+
+bot.help((ctx) => ctx.reply(command_text.commands));
+
+// bot.command("questions", (ctx) => {});
+
+
+
+bot.launch(); //start bot
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
