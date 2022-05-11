f7bbee6c921099ed701f9a4062fd9cebbd636799
SemenovYakov <d.simson241100@yandex.ru>
2022-05-11 21:16:33 +0400

    second

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const command_text = require("./components/constants");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => {
-  ctx.replyWithSticker('CAACAgIAAxkBAAEEr-5ieQL1WxfzAqTkp2h4abIxHlLVJQACdAwAAra7OEuOzqfMQMSAJSQE')
+  ctx.replyWithSticker(
+    "CAACAgIAAxkBAAEEr-5ieQL1WxfzAqTkp2h4abIxHlLVJQACdAwAAra7OEuOzqfMQMSAJSQE"
+  );
+  ctx.reply(
+    ctx.message.from.first_name && ctx.message.from.last_name
+      ? `Привет, ${
+          ctx.message.from.first_name + " " + ctx.message.from.last_name
+        }!`
+      : `Привет, незнакомец!}`
+  );
   console.log(ctx.message);
 });
 
 bot.help((ctx) => ctx.reply(command_text.commands));
 
 // bot.command("questions", (ctx) => {});
-
-
 
 bot.launch(); //start bot
 // Enable graceful stop
